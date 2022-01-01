3bf96a5e9516800c0e0591a74abd101278808948
inciner8r <punarvpawade17@gmail.com>
2022-01-02 00:04:06 +0530

    start to build server

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	var name string = "ok "
-	var age = 13
-	fmt.Println(name)
-	fmt.Printf("%T", age)
+	fmt.Println("ok")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
