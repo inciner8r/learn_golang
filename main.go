439bde282e2ff3a137a3fe901e59a92d6ca103d9
inciner8r <punarvpawade17@gmail.com>
2022-01-02 00:26:29 +0530

    handler for /

--- main.go
+++ main.go
@@ -5,8 +5,22 @@ import (
 	"net/http"
 )
 
+func rootHandler(rw http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(rw, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(rw, "unsupported method", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(rw, "kekw")
+}
+
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) { fmt.Fprintf(rw, "server up") })
+	http.HandleFunc("/", rootHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
