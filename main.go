420a2c4e4fd95490cca03967e6315ee3546021b9
inciner8r <punarvpawade17@gmail.com>
2022-02-26 14:59:43 +0530

    deck

--- main.go
+++ main.go
@@ -1,67 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-type Coaster struct {
-	Name         string
-	Manufacturer string
-	ID           string
-	InPark       string
-	Height       string
-}
-
-type coastersHandlers struct {
-	store map[string]Coaster
-}
-
-func (h *coastersHandlers) get(rw http.ResponseWriter, r *http.Request) {
-
-}
-
-func newCoastersHandler() *coastersHandlers {
-	return &coastersHandlers{
-		store: map[string]Coaster{},
-	}
-}
-
-func rootHandler(rw http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(rw, "404 not found", http.StatusNotFound)
-		return
-	}
-
-	if r.Method != "GET" {
-		http.Error(rw, "unsupported method", http.StatusNotFound)
-		return
-	}
-
-	fmt.Fprintf(rw, "kekw")
-}
-
-func formHandler(rw http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprint(rw, "parseForm er: %v", err)
-		return
-	}
-	fmt.Printf("post successful")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-
-	fmt.Fprintf(rw, "name:%s \naddress:%s", name, address)
-}
-
 func main() {
-	coastersHandlers := newCoastersHandler()
-	FileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", FileServer)
-	http.HandleFunc("/coaster", coastersHandlers.get)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", rootHandler)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
-	}
+	cards := newDeck()
+	cards.print()
 }
