e5c33b2fcd621eae5bc10b2f42aed270f3996676
inciner8r <punarvpawade17@gmail.com>
2022-03-01 17:05:23 +0530

    rendom

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 func main() {
-	cards := newDeckFromFile("my_cards")
+	cards := newDeck()
 	// hand, remainingCards := deal(cards, 5)
 	// hand.print()
 	// remainingCards.print()
 	// fmt.Println(cards.toString())
+	cards.shuffle()
 	cards.print()
 }
