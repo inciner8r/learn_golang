06609e366bb31e55009bd9eebc15164436ae0233
inciner8r <punarvpawade17@gmail.com>
2022-03-01 15:50:04 +0530

    more

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 func main() {
-	cards := newDeck()
+	cards := newDeckFromFile("my_cards")
 	// hand, remainingCards := deal(cards, 5)
 	// hand.print()
 	// remainingCards.print()
 	// fmt.Println(cards.toString())
-	cards.saveToFile("my_cards")
+	cards.print()
 }
