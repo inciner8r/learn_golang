678107c4e77f92ce9175fd1348703c26429b1d24
inciner8r <punarvpawade17@gmail.com>
2022-03-01 22:55:50 +0530

    more test

--- deck_test.go
+++ deck_test.go
@@ -7,4 +7,10 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("deck length is %v", len(d))
 	}
+	if d[0] != "Ace of Spades" {
+		t.Errorf("first card is %v", d[0])
+	}
+	if d[len(d)-1] != "Four of Clover" {
+		t.Errorf("last card is %v", d[len(d)-1])
+	}
 }
