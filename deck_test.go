fcc10664c10e71a4f7cb38bd63baf797a081915e
inciner8r <punarvpawade17@gmail.com>
2022-03-02 12:02:17 +0530

    testsssss

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -13,4 +16,16 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clover" {
 		t.Errorf("last card is %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Deck length is %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
