52f0896a409905595ba0ea2b6b42d467f2ad8b18
inciner8r <punarvpawade17@gmail.com>
2022-03-10 01:13:01 +0530

    html template render

--- main.go
+++ main.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "home page ")
+	renderTemplate(w, "home.page.tmpl")
 }
 func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "About page")
-}
-func AddValues(x, y int) int {
-	return x + y
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 	http.ListenAndServe(":3000", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template: ", err)
+		return
+	}
 }
