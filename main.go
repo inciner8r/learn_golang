55f83c07c49d698818f016ac512e0d8efb06b6b3
inciner8r <punarvpawade17@gmail.com>
2022-03-10 00:22:47 +0530

    sarvar

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "home page ")
+}
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About page")
+}
+func AddValues(x, y int) int {
+	return x + y
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.ListenAndServe(":3000", nil)
+}
